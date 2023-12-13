fb769ca4c80d62a259faf5cf3045b21c7a61d744
shigetaaa <acrosstanabata@gmail.com>
2023-12-13 16:46:05 +0900

    関数の定義　関数の表示を追加しました。

--- quest_3/quest_3.php
+++ quest_3/quest_3.php
@@ -8,27 +8,25 @@
 
 <body>
 
-  <!-- 数字を1から100まで順番に出力する -->
-  <p>
-    <?php for ($i = 1; $i <= 100; $i++) {
-      echo $i;
-    } ?>
-  </p>
-
 
   <p><?php
-      //数字を1から100まで順番に出力する
-      for ($i = 1; $i <= 100; $i++) {
-        if ($i % 3 === 0 && $i % 5 === 0) { //3かつ5の倍数の時「FizzBuzz」と出力する
-          echo "{$i}.FizzBuzz" . "</br>";
-        } elseif ($i % 3 === 0) { //3の倍数の時「Fizz」と出力する
-          echo "{$i}.Fizz" . "</br>";
-        } elseif ($i % 5 === 0) { //5の倍数の時「Buzz」と出力する
-          echo "{$i}.Buzz" . "</br>";
-        } else {
-          echo $i . "</br>";
+      function fizz_buzz()
+      {
+        //数字を1から100まで順番に出力する
+        for ($i = 1; $i <= 100; $i++) {
+          if ($i % 3 === 0 && $i % 5 === 0) { //3かつ5の倍数の時「FizzBuzz」と出力する
+            echo "{$i}.FizzBuzz" . "</br>";
+          } elseif ($i % 3 === 0) { //3の倍数の時「Fizz」と出力する
+            echo "{$i}.Fizz" . "</br>";
+          } elseif ($i % 5 === 0) { //5の倍数の時「Buzz」と出力する
+            echo "{$i}.Buzz" . "</br>";
+          } else {
+            echo $i . "</br>";
+          }
         }
       }
+
+      echo fizz_buzz();
       ?>
   </p>
 
