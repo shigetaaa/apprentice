be146f3be5bf0efebd5a1b1fdae892ae12a26609
shigetaaa <acrosstanabata@gmail.com>
2023-12-15 17:54:33 +0900

    クエスト6を提出

---
+++ quest_6/quest_6.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>変数を宣言し代入することができる</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    function greater($x, $y){
+      if($x)
+
+    }
+
+    ?>
+
+
+
+
+  </p>
+
+
+
+</body>
+
+</html>
